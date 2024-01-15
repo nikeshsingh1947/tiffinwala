63a35267a5d3ec9461f7561f63fe16bc6cef75e1
nikeshsingh1947 <141706194+nikeshsingh1947@users.noreply.github.com>
2024-01-15 17:04:09 +0530

    Update Payment.controller.js

--- src/controller/Payment.controller.js
+++ src/controller/Payment.controller.js
@@ -99,7 +99,7 @@ const paymentVerification = async (req, res) => {
     });
 
     res.redirect(
-      `http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`
+      `https://delhitiffinwala.com/paymentsuccess?reference=${razorpay_payment_id}`
     );
   } else {
     res.status(400).json({
